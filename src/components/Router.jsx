fd5c71b05293c790cb5b6953a25bc4adc3c6b601
Richard Lois <richard.lois@ti.ukdw.ac.id>
2023-10-21 22:25:04 +0700

    Create component and page for edit profile

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -4,19 +4,23 @@ import Blank from "../pages/Blank";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
+import Profile from "../pages/Profile/Profile";
+import EditPicture from "../pages/Profile/EditPicture";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<AppLayout />}>
-                <Route index element={<Home />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/bookmark" element={<Blank />} />
-                <Route path="/logout" element={<Blank />} />
+                    <Route index element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/bookmark" element={<Blank />} />
+                    <Route path="/logout" element={<Blank />} />
                 </Route>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/edit-profile-pic" element={<EditPicture />} />
             </Routes>
         </BrowserRouter>
     );

02bf2a4abd55c15be606db55baf71cd3c155b5fc
Richard Lois <richard.lois@ti.ukdw.ac.id>
2023-10-22 13:19:54 +0700

    Add navigation on login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,19 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-
 import React, { useState } from "react";
 import "./login.css";
 import logo from "../../assets/logo.svg";
 import eyeOn from "../../assets/eye.svg";
 import eyeOff from "../../assets/eye-off.svg";
+import { useNavigate } from "react-router";
 
 export const Login = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const navigate = useNavigate();
+  const clickRegister = () => {
+    navigate("/register");
+  };
+  const clickLogin = () => {
+    navigate("/");
+  };
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
@@ -37,10 +42,10 @@ export const Login = () => {
               />
             </div>
             
-            <button className="signup-button">Sign in</button>
+            <button className="signup-button" onClick={clickLogin}>Sign in</button>
             <div className="text-wrapper-2">
               New to MovieHub?{" "}
-              <span className="text-wrapper-3">Sign Up now.</span>
+              <span className="text-wrapper-3" onClick={clickRegister}>Sign Up now.</span>
             </div>
           </div>
         </div>
@@ -48,29 +53,4 @@ export const Login = () => {
     </div>
   );
 };
-
-function Login() {
-    return (
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-  
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    );
-  }
 export default Login;

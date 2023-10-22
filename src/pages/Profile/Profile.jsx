923664664e5e1d42373dcc2d9a8dc4880bf57824
Richard Lois <richard.lois@ti.ukdw.ac.id>
2023-10-22 11:54:36 +0700

    Add button back to home

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -5,6 +5,7 @@ import { Row, Col, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ButtonBackHome from "../../components/Profile/ButtonBackHome";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -31,6 +32,7 @@ const Profile = () => {
     <div>
       <ToastContainer />
       <div className="header-profile">
+        <ButtonBackHome />
         <ProfilePicture />
       </div>
 

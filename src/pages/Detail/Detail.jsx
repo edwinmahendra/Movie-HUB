53d241bf4b5ac4738f42fd8f9d14dccfdbd7df51
Edwin Mahendra <edwin.mahendra@ti.ukdw.ac.id>
2023-12-15 16:15:37 +0700

    Update detail.jsx

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -13,40 +13,21 @@ import SearchBar from "../../components/Movie/SearchBar";
 import bookmark1 from "../../assets/bookmark.svg";
 import bookmark2 from "../../assets/bookmark-off.svg";
 import ButtonBackHome from "../../components/Profile/ButtonBackHome";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import { getAuth } from "firebase/auth";
-import {
-  collection,
-  deleteDoc,
-  doc,
-  getDoc,
-  addDoc,
-  updateDoc,
-  getFirestore,
-  setDoc,
-} from "firebase/firestore";
-import SearchResults from "../../components/Search/SearchResults";
+import { collection, deleteDoc, doc, getDoc, addDoc,updateDoc, getFirestore,setDoc } from "firebase/firestore";
 
 export const Detail = () => {
-  const { idMovie } = useParams();
-  const [userScore, setUserScore] = useState();
-  const [genres, setGenres] = useState([]);
-  const [videos, setVideos] = useState([]);
-  const [directors, setDirectors] = useState([]);
-  const [casts, setCasts] = useState([]);
-  const [recommendations, setRecommendations] = useState([]);
-  const [searchTerm, setSearchTerm] = useState(""); // State to capture search term
-  const [searchResults, setSearchResults] = useState([]); // State to store search results
-  const [isSearching, setIsSearching] = useState(false);
-  const navigate = useNavigate();
   const [isBookmarked, setIsBookmarked] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true); 
   const [dataMovies, setDataMovies] = useState();
   const db = getFirestore();
   const auth = getAuth();
 
   const toggleBookmark = async () => {
+    const db = getFirestore();
+    const auth = getAuth();
     const user = auth.currentUser;
     const userRef = collection(db, "Users", user.uid, "Bookmarks");
     try {
@@ -55,16 +36,16 @@ export const Detail = () => {
         console.log("No user is currently signed in.");
         return;
       }
-
+  
       const userId = user.uid;
       const movieId = idMovie;
-
-      const bookmarkRef = doc(db, "Users", userId, "Bookmarks", movieId);
+  
+      const bookmarkRef = doc(db, 'Users', userId, 'Bookmarks', movieId);
       const bookmarkDocSnapshot = await getDoc(bookmarkRef);
-
+  
       if (bookmarkDocSnapshot.exists()) {
         await deleteDoc(bookmarkRef);
-        console.log("Movie removed from bookmarks");
+        console.log('Movie removed from bookmarks');
       } else {
         await setDoc(bookmarkRef, {
           idMovie: movieId,
@@ -72,18 +53,26 @@ export const Detail = () => {
           releaseDate: dataMovies.release_date,
           sinopsis: dataMovies.overview,
           genre: genres.join(", "),
-          posterPath:
-            process.env.REACT_APP_BASE_URL_IMG_MOVIE + dataMovies.poster_path,
+          posterPath: process.env.REACT_APP_BASE_URL_IMG_MOVIE + dataMovies.poster_path,
           dateAdded: new Date().toISOString(),
         });
-        console.log("Movie added to bookmarks");
+        console.log('Movie added to bookmarks');
       }
-
       setIsBookmarked(!bookmarkDocSnapshot.exists());
     } catch (error) {
       console.error("Error toggling bookmark:", error);
     }
   };
+  
+
+  
+  const { idMovie } = useParams();
+  const [userScore, setUserScore] = useState();
+  const [genres, setGenres] = useState([]);
+  const [videos, setVideos] = useState([]);
+  const [directors, setDirectors] = useState([]);
+  const [casts, setCasts] = useState([]);
+  const [recommendations, setRecommendations] = useState([]);
 
   const config = {
     headers: { Authorization: `Bearer ${process.env.REACT_APP_MOVIE_TOKEN}` },
@@ -94,37 +83,37 @@ export const Detail = () => {
     getVideosUrl();
     getDirectorsCasts();
     getRecommendations();
+    const fetchBookmarks = async () => {
+      try {
+        const db = getFirestore();
+        const user = auth.currentUser;
+        if (!user) {
+          console.log("No user is currently signed in.");
+          return;
+        }
+  
+        const userId = user.uid;
+        const movieId = idMovie;
+  
+        const bookmarkRef = doc(db, 'Users', userId, 'Bookmarks', movieId);
+        const bookmarkDocSnapshot = await getDoc(bookmarkRef);
+        if (bookmarkDocSnapshot.exists()) {
+          setIsBookmarked(true)
+          await deleteDoc(bookmarkRef);
+          console.log('Movie removed from bookmarks');
+        } else {
+          setIsBookmarked(false)
+        }
+        const initialIsBookmarked = bookmarkDocSnapshot.exists();
+        setIsBookmarked(initialIsBookmarked);
+      } catch (error) {
+        console.error("Error checking bookmark:", error);
+      }
+    };
+  
     fetchBookmarks();
+  
   }, [idMovie, db, auth]);
-
-  const fetchBookmarks = async () => {
-    try {
-      const user = auth.currentUser;
-      if (!user) {
-        console.log("No user is currently signed in.");
-        return;
-      }
-
-      const userId = user.uid;
-      const movieId = idMovie;
-
-      const bookmarkRef = doc(db, "Users", userId, "Bookmarks", movieId);
-      const bookmarkDocSnapshot = await getDoc(bookmarkRef);
-      if (bookmarkDocSnapshot.exists()) {
-        setIsBookmarked(true);
-        await deleteDoc(bookmarkRef);
-        console.log("Movie removed from bookmarks");
-      } else {
-        setIsBookmarked(false);
-      }
-
-      const initialIsBookmarked = bookmarkDocSnapshot.exists();
-
-      setIsBookmarked(initialIsBookmarked);
-    } catch (error) {
-      console.error("Error checking bookmark:", error);
-    }
-  };
 
   const getDetailMovie = async () => {
     try {
@@ -146,69 +135,56 @@ export const Detail = () => {
   };
 
   const getVideosUrl = async () => {
-    try {
-      const res = await axios
-        .get(
-          `${process.env.REACT_APP_BASE_URL_MOVIE}movie/${idMovie}/videos`,
-          config
-        )
-        .then((res) => {
-          let videos = res.data.results.map((video) => video.key);
-          setVideos(videos);
-        });
-    } catch (err) {
+    try{
+      const res = await axios.get(
+        `${process.env.REACT_APP_BASE_URL_MOVIE}movie/${idMovie}/videos`,
+        config
+      ).then((res) => {
+        let videos = res.data.results.map((video) => video.key);
+        setVideos(videos);
+      });
+    }catch(err){
       console.log(err);
     }
   };
 
   const getDirectorsCasts = async () => {
-    try {
-      const res = await axios
-        .get(
-          `${process.env.REACT_APP_BASE_URL_MOVIE}movie/${idMovie}/credits`,
-          config
-        )
-        .then((res) => {
-          let directors = res.data.crew.filter(
-            (crew) => crew.job === "Director"
-          );
-          directors = directors.map((director) => director.name);
-          let casts = res.data.cast;
-          setDirectors(directors);
-          setCasts(casts);
-        });
-    } catch (err) {
+    try{
+      const res = await axios.get(
+        `${process.env.REACT_APP_BASE_URL_MOVIE}movie/${idMovie}/credits`,
+        config
+      ).then((res) => {
+        let directors = res.data.crew.filter((crew) => crew.job === "Director");
+        directors = directors.map((director) => director.name);
+        let casts = res.data.cast;
+        setDirectors(directors);
+        setCasts(casts);
+      });
+    }catch(err){
       console.log(err);
-    }
-  };
-
-  const handleSearch = (query) => {
-    if (query.trim()) {
-      navigate(`/home?search=${encodeURIComponent(query)}`); // Redirect to home with query using navigate
     }
   };
 
   const getRecommendations = async () => {
-    try {
-      const res = await axios
-        .get(
-          `${process.env.REACT_APP_BASE_URL_MOVIE}movie/${idMovie}/similar`,
-          config
-        )
-        .then((res) => {
-          let recommendations = res.data.results;
-          setRecommendations(recommendations);
-          console.log("recommendations", recommendations);
-        });
-    } catch (err) {
+    try{
+      const res = await axios.get(
+        `${process.env.REACT_APP_BASE_URL_MOVIE}movie/${idMovie}/similar`,
+        config
+      ).then((res) => {
+        let recommendations = res.data.results;
+        setRecommendations(recommendations);
+        console.log('recommendations', recommendations);
+      });
+    }catch(err){
       console.log(err);
     }
+  };
 
-    if (isLoading) {
-      return <div>Loading...</div>;
-    }
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    return (
+  return (
       <div className="detail-movie">
         <div className="div">
           <div className="box">
@@ -216,19 +192,12 @@ export const Detail = () => {
               <ButtonBackHome />
             </div>
             <div className="group">
-              <SearchBar onSearch={handleSearch} />
+              <SearchBar />
             </div>
           </div>
           <div className="overlap-group">
             <div className="rectangle" />
-            <img
-              className="main-image rounded"
-              alt="main image"
-              src={
-                process.env.REACT_APP_BASE_URL_IMG_MOVIE +
-                dataMovies.poster_path
-              }
-            />
+            <img className="main-image rounded" alt="main image" src={process.env.REACT_APP_BASE_URL_IMG_MOVIE + dataMovies.poster_path} />
             <img
               className="bookmark-icon"
               alt="Bookmark Icon"
@@ -237,59 +206,47 @@ export const Detail = () => {
             />
             <p className="header-movie">
               <span className="text-wrapper">{dataMovies.original_title}</span>
-              <span className="span">
-                {" "}
-                {`(${dataMovies.release_date.slice(0, 4)})`}
-              </span>
+              <span className="span"> {`(${dataMovies.release_date.slice(0,4)})`}</span>
             </p>
             <div className="text-wrapper-2">{`${userScore}% User Score`}</div>
             <div className="text-wrapper-3">Creator</div>
             <div className="text-wrapper-4">{directors.join(", ")}</div>
-            <p className="sci-fi-fantasy-drama">{genres.join(", ")}</p>
+            <p className="sci-fi-fantasy-drama">
+              {genres.join(", ")}
+            </p>
             <div className="text-wrapper-6">{`${dataMovies.release_date} |`}</div>
           </div>
           <div className="text-wrapper-8">Movie Overview</div>
-          <p className="seven-noble-families">{dataMovies.overview}</p>
+          <p className="seven-noble-families">
+            {dataMovies.overview}
+          </p>
           <div class="cast-section">
             <h2 class="cast-label">Cast and Crew</h2>
             <div class="container-cast">
               {casts.map((cast) => {
-                return (
-                  <CastItem
-                    actorName={cast.name}
-                    character={cast.character}
-                    profilePict={cast.profile_path}
-                  />
-                );
+                return <CastItem actorName={cast.name} character={cast.character} profilePict={cast.profile_path} />;
               })}
             </div>
           </div>
           <div class="trailer-section">
             <h2 class="trailer-label">Trailer Movie</h2>
             <div class="container-trailer">
-              {videos.map((youtubeKey) => {
-                return <TrailerItem youtubeKey={youtubeKey} />;
-              })}
+              {
+                videos.map((youtubeKey) => {
+                  return <TrailerItem youtubeKey={youtubeKey} />
+                })
+              }
             </div>
           </div>
           <hr class="line" />
           <div class="recommendation-section">
             <h2 class="recommendation-label">Recommendation</h2>
             <div class="container-recommendation">
-              {recommendations.map((recommendation) => {
-                return (
-                  <RecommendationItem
-                    movieId={recommendation.id}
-                    title={recommendation.original_title}
-                    pictUrl={
-                      recommendation.backdrop_path == null
-                        ? recommendation.poster_path
-                        : recommendation.backdrop_path
-                    }
-                    releaseDate={recommendation.release_date}
-                  />
-                );
-              })}
+              {
+                recommendations.map((recommendation) => {
+                  return <RecommendationItem movieId={recommendation.id} title={recommendation.original_title} pictUrl={recommendation.backdrop_path == null ? recommendation.poster_path : recommendation.backdrop_path} releaseDate={recommendation.release_date} />
+                })
+              }
             </div>
           </div>
           <div className="footer-box">
@@ -308,11 +265,7 @@ export const Detail = () => {
               />
               <img className="icon-youtube" alt="Youtube icon" src={youtube} />
               <img className="icon-twitter" alt="Twitter icon" src={twitter} />
-              <img
-                className="icon-facebook"
-                alt="Facebook icon"
-                src={facebook}
-              />
+              <img className="icon-facebook" alt="Facebook icon" src={facebook} />
             </div>
             <div className="footer-right">
               <p className="footer-text-wrapper">© 2023 - AGB Company.</p>
@@ -320,6 +273,5 @@ export const Detail = () => {
           </div>
         </div>
       </div>
-    );
-  };
+  )
 };
